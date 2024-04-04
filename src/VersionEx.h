1c8477c483e200207e2e52c7e2afaaa8380379f1
gmh5225 <2315157@qq.com>
2024-04-05 00:41:42 +0800

    ver

---
+++ src/VersionEx.h
@@ -0,0 +1,22 @@
+// encoding: UTF-8
+// //////////////////////////////////////////////////////////
+//                                                         //
+// this file is generated - use template to apply changes  //
+//                                                         //
+// //////////////////////////////////////////////////////////
+#define APPNAME Notepad3
+#define SAPPNAME "Notepad3"
+#define VERSION_MAJOR 6
+#define VERSION_MINOR 24
+#define VERSION_REV 405
+#define VERSION_BUILD 1
+#define SCINTILLA_VER 542
+#define LEXILLA_VER 531
+#define ONIGURUMA_REGEX_VER 6.9.9
+#define UCHARDET_VER 2018.09.27
+#define TINYEXPR_VER 2018.05.11
+#define UTHASH_VER 2.3.0
+#define VERSION_PATCH 
+#define VERSION_COMMIT_ID 9c13b2fa
+
+#include "Version.h"
